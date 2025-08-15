5b8a5c8bc6fd2329b2fe456b44e3f9656eb98486
Amrutha-NV <amruthavasudeva28@gmail.com>
2025-08-15 16:43:35 +0530

    implemented basic crud operations for a to do web-page

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
 //creating a arr of objects for the tasks
 let tasks = [{
     id: uuidv4(),
@@ -29,14 +31,37 @@ let tasks = [{
     description: "gather question papers",
 }];
 app.get("/home", (req, res) => {
-    res.send("get request successfull");
+    res.render("home.ejs", { tasks });
+});
+app.post("/newtask", (req, res) => {
+    let id = uuidv4();
+    let { title, description } = req.body;
+    tasks.push({ id, title, description });
+    console.log(tasks);
+    res.redirect("/home");
+});
+app.get("/home/newtask", (req, res) => {
+    res.render("newtask.ejs");
+});
+app.patch("/home/:id", (req, res) => {
+    let { id } = req.params;
+    let task = tasks.find((t) => (id == t.id));
+    task.title = req.body.title;
+    task.description = req.body.description;
+    res.redirect("/home");
+
 })
-
-
-
-
-
-
+app.get("/home/:id/edit", (req, res) => {
+    let { id } = req.params;
+    let task = tasks.find((t) => (id == t.id));
+    console.log(task);
+    res.render("edit.ejs", { task });
+});
+app.delete("/home/:id", (req, res) => {
+    let { id } = req.params;
+    tasks = tasks.filter((t) => (id != t.id));
+    res.redirect("/home");
+})
 app.listen(port, (req, res) => {
     console.log(`listening at port ${port}`);
 });
